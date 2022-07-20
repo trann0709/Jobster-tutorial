ccd8834d380de462996319926044078dd4bf3f01
Ngan Tran <trann0709@gmail.com>
2022-07-19 20:02:18 -0500

    set up router and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Landing, Dashboard, Error, Register } from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <h1>jobster</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="landing" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/Landing.js
@@ -0,0 +1,33 @@
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit iure sit voluptatibus mollitia vitae aliquid enim,
+            commodi adipisci assumenda quibusdam?
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
