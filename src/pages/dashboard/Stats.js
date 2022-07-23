61f9c8fce241fd80feb5a9d56e3967d90ff15039
Ngan Tran <trann0709@gmail.com>
2022-07-22 19:42:43 -0500

    created Job Thunk and refactored

--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { showStats } from "../../features/allJobs/allJobsSlice";
+
 const Stats = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(showStats());
+  }, []);
+
+  const { isLoading, monthlyApplications } = useSelector(
+    (store) => store.allJobs
+  );
+
+  if (isLoading) {
+    return <Loading center />;
+  }
+
   return <div>Stats</div>;
 };
 
